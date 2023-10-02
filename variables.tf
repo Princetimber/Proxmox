f6462f8c829caf90f401ba88659e571efae6d6cb
Olamide Olaleye <olamide.olaleye@outlook.com>
2023-10-02 12:04:47 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "pm_api_token_id" {
+  default   = "zadmin@pam!new_token"
+  sensitive = true
+}
+
+variable "pm_api_url" {
+  default = "https://10.0.2.3:8006/api2/json"
+}
+variable "pm_api_token_secret" {
+  default   = "69e17121-5955-4959-b6eb-e8420bc69d07"
+  sensitive = true
+}
